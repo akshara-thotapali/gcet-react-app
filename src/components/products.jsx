6097d72f283504474ee3b045ef576e1fe1a6bfc7
akshara-thotapali <thotapalliakshara2@gmail.com>
2025-06-05 11:12:59 +0530

    update

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -6,7 +6,7 @@ export default function Product() {
   const { user } = useContext(AppContext);
   const [products, setProducts] = useState([]);
   const fetchProducts = async () => {
-    const res = await axios.get("http://localhost:8080/products");
+    const res = await axios.get("https://gcet-node-app-ashen.vercel.app/products/all");
     setProducts(res.data);
   };
   useEffect(() => {
