dccb69b8169f04bbdc01de82901d93b66058d814
akshara-thotapali <thotapalliakshara2@gmail.com>
2025-06-06 10:39:19 +0530

    update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,17 +2,24 @@ import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AppContext } from "../App";
 import axios from "axios";
-import "./Product.css"
+import "./Product.css";
 export default function Product() {
-  const { user } = useContext(AppContext);
-  const [products, setProducts] = useState([]);
+  const { user, products, setProducts, cart, setCart } = useContext(AppContext);
+  // const [products, setProducts] = useState([]);
+  const API = import.meta.env.VITE_API_URL;
   const fetchProducts = async () => {
-    const res = await axios.get(`https://gcet-node-app-ashen.vercel.app/products/all`);
+    const res = await axios.get(`${API}/products/all`);
     setProducts(res.data);
   };
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  const addToCart = (id) => {
+    !cart[id] && setCart({ ...cart, [id]: 1 });
+    
+  };
+
   return (
     <div>
       <h3>Welcome {user.name}! </h3>
@@ -22,7 +29,7 @@ export default function Product() {
             <div key={value._id}>
               <h3>{value.name}</h3>
               <h4>{value.price}</h4>
-              <button>Add to Cart</button>
+              <button onClick={() => addToCart(value.pid)}>Add to Cart</button>
             </div>
           ))}
       </div>
